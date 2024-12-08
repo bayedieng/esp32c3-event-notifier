c510476514cd14b20edd9bf783b812aa0072287a
Baye Dieng <bayedieng98@gmail.com>
2024-12-08 13:08:39 +0000

    connect to wifi

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-use esp_idf_hal::delay::FreeRtos;
+use std::time::Duration;
 
-fn main() {
-    println!("Hello World");
-    loop {
-        FreeRtos::delay_ms(1000);
-    }
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::prelude::*;
+use esp_idf_svc::hal::sys::EspError;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::wifi::{BlockingWifi, Configuration, EspWifi};
+
+const SSID: &str = env!("WIFI_SSID");
+const PASSWORD: &str = env!("WIFI_PASS");
+
+fn connect_to_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> Result<(), EspError> {
+    let wifi_configuration = Configuration::Client(esp_idf_svc::wifi::ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: esp_idf_svc::wifi::AuthMethod::WPA2Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+    wifi.set_configuration(&wifi_configuration)?;
+    wifi.start()?;
+    println!("Wifi Started");
+    wifi.connect()?;
+    println!("Connected to wifi");
+    wifi.wait_netif_up()?;
+    println!("Wifi network is up");
+    Ok(())
+}
+
+fn main() -> Result<(), EspError> {
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+    let inner_wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
+    let mut wifi = BlockingWifi::wrap(inner_wifi, sys_loop)?;
+    connect_to_wifi(&mut wifi)?;
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    println!("Wifi DHCP info: {ip_info:?}");
+    std::thread::sleep(Duration::from_secs(5));
+    Ok(())
 }
