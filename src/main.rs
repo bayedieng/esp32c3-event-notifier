0a7ca7319fc824153f5d1d56cc827e01bec0dbcf
Baye Dieng <bayedieng98@gmail.com>
2024-12-21 15:11:35 +0000

    basic wifi stuff

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,37 @@
-use std::time::Duration;
+#![no_std]
+#![no_main]
 
-use esp_idf_svc::eventloop::EspSystemEventLoop;
-use esp_idf_svc::hal::prelude::*;
-use esp_idf_svc::hal::sys::EspError;
-use esp_idf_svc::nvs::EspDefaultNvsPartition;
-use esp_idf_svc::wifi::{BlockingWifi, Configuration, EspWifi};
-
-const SSID: &str = env!("WIFI_SSID");
-const PASSWORD: &str = env!("WIFI_PASS");
-
-fn connect_to_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> Result<(), EspError> {
-    let wifi_configuration = Configuration::Client(esp_idf_svc::wifi::ClientConfiguration {
-        ssid: SSID.try_into().unwrap(),
-        bssid: None,
-        auth_method: esp_idf_svc::wifi::AuthMethod::WPA2Personal,
-        password: PASSWORD.try_into().unwrap(),
-        channel: None,
-        ..Default::default()
-    });
-    wifi.set_configuration(&wifi_configuration)?;
-    wifi.start()?;
-    println!("Wifi Started");
-    wifi.connect()?;
-    println!("Connected to wifi");
-    wifi.wait_netif_up()?;
-    println!("Wifi network is up");
-    Ok(())
+use core::panic::PanicInfo;
+use esp_hal::{prelude::*, rng::Rng, timer::timg::TimerGroup};
+use esp_wifi::wifi::WifiStaDevice;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    esp_println::println!(
+        "Panicked at: {:?}\n Line: {:?}",
+        info.message(),
+        info.location().unwrap().line()
+    );
+    loop {}
 }
 
-fn main() -> Result<(), EspError> {
-    let peripherals = Peripherals::take()?;
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-    let inner_wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
-    let mut wifi = BlockingWifi::wrap(inner_wifi, sys_loop)?;
-    connect_to_wifi(&mut wifi)?;
-    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-    println!("Wifi DHCP info: {ip_info:?}");
-    std::thread::sleep(Duration::from_secs(5));
-    Ok(())
+#[entry]
+fn main() -> ! {
+    let config = esp_hal::Config::default();
+    let peripherals = esp_hal::init(config);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let mut wifi_device = peripherals.WIFI;
+    let wifi_init = esp_wifi::init(
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+    let (iface, wifi_device, mut wifi_controller) =
+        esp_wifi::wifi::utils::create_network_interface(
+            &wifi_init,
+            &mut wifi_device,
+            WifiStaDevice,
+        )
+        .unwrap();
+    loop {}
 }
