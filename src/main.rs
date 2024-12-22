7b2168080ce61245c5de431f2722b4249970a9f0
Baye Dieng <bayedieng98@gmail.com>
2024-12-22 16:04:38 +0000

    some stuff

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use core::panic::PanicInfo;
 use esp_hal::{prelude::*, rng::Rng, time, timer::timg::TimerGroup};
 use esp_println::println;
 use esp_wifi::wifi::{ClientConfiguration, Configuration, WifiStaDevice};
-use smoltcp::iface::{SocketSet, SocketStorage};
 use smoltcp::socket::tcp::{Socket, SocketBuffer};
 
 const SSID: &str = env!("SSID");
 const WIFI_PASS: &str = env!("WIFI_PASS");
 const SOCKET_BUF_SIZE: usize = 1024;
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     esp_println::println!(
@@ -33,7 +33,7 @@ fn main() -> ! {
 
     let wifi_init = esp_wifi::init(timg0.timer0, rng, peripherals.RADIO_CLK).unwrap();
 
-    let (yiface, wifi_device, mut wifi_controller) =
+    let (iface, wifi_device, mut wifi_controller) =
         esp_wifi::wifi::utils::create_network_interface(
             &wifi_init,
             &mut wifi_device,
@@ -57,13 +57,11 @@ fn main() -> ! {
     } else {
         println!("Could not connect to Wifi");
     }
+    let mut rx_buffer = [0; SOCKET_BUF_SIZE];
+    let mut tx_buffer = [0; SOCKET_BUF_SIZE];
+    let tcp_rx_buffer = SocketBuffer::new(&mut rx_buffer[..]);
+    let tcp_tx_buffer = SocketBuffer::new(&mut tx_buffer[..]);
+    let tcp_socket = Socket::new(tcp_rx_buffer, tcp_tx_buffer);
 
-    let mut rx_buf = [0u8; 1024];
-    let mut tx_buf = [0u8; 1024];
-    let mut rx_socket_buf = SocketBuffer::new(&mut rx_buf[..]);
-    let mut tx_socket_buf = SocketBuffer::new(&mut tx_buf[..]);
-    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
-    let mut socket_set = SocketSet::new(&mut socket_set_entries[..]);
-    let mut tcp_socket = Socket::new(rx_socket_buf, tx_socket_buf);
     loop {}
 }
